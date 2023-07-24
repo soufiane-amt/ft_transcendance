24ab8fcaac27ed3692ac550899e69b4663341037
Soufiane Amajat <samajat@e3r3p16.1337.ma>
2023-07-24 10:50:34 +0100

    first hello world from user1 to user2 in DM chat service

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,1 +1,8 @@
-export class CreateChatDto {}
+export interface CreateMessageDto 
+{
+    send_id :string
+    created_at : Date
+    channel_id : string
+    content : string
+    is_read : boolean
+}
