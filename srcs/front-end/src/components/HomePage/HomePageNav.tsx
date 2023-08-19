4959cb6d38f6ee569174adaa0d1964829370eb6b
Abdellah BELLAKRIM <bellakrim2032@gmail.com>
2023-08-19 22:08:26 +0100

    finish navbar component in homepage

--- srcs/front-end/src/components/HomePage/HomePageNav.tsx
+++ srcs/front-end/src/components/HomePage/HomePageNav.tsx
@@ -11,19 +11,19 @@ export default function HomePageNav() {
 
 
   return (
-    <nav className="bg-[#0D0149]  w-full h-[100px] flex justify-between items-center">
-        <Image src="/myWhiteLogo.png" width={109.5} height={85.7} alt="Logo" priority={true} className="px-[15px] lg:[50px]"/>
-      <div className="lg:flex hidden items-center space-x-20 text-white ml-[50px] mr-[50px] font-bold ">
-        <Link href="/" className="bg-[#333989] px-[60px] py-[10px] rounded-lg hover:bg-white hover:text-black">Home</Link>
-        <Link href="/history" className="bg-[#333989] px-[60px] py-[10px] rounded-lg hover:bg-white hover:text-black">History</Link>
-        <Link href="/about" className="bg-[#DA343E] px-[60px] py-[10px] rounded-lg hover:bg-white hover:text-black">About</Link>
+    <nav className="bg-[#0D0149]  w-full h-[100px] flex justify-between items-center fixed">
+      <Image src="/myWhiteLogo.png" width={129.5} height={105.7} alt="Logo" priority={true} className="px-[15px] lg:[50px]"/>
+      <div className="lg:flex hidden items-center space-x-20 text-white ml-[50px] mr-[20px] font-bold">
+        <Link href="/" className="bg-[#333989] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#6a70c8] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#333989]">Home</Link>
+        <Link href="/history" className="bg-[#333989] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#6a70c8] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#333989]">History</Link>
+        <Link href="/about" className="bg-[#DA343E] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#e36870] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#DA343E]">About</Link>
       </div>
       <div className="lg:hidden flex flex-1 justify-end items-center">
           <Image src={toggle ? close : open} width={toggle ? 32 : 45} height={toggle ? 32 : 45} alt="Menu" className="mr-[20px] object-contain" onClick={() => setToggle((prev) => !prev)} />
-          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-[#000000] bg-opacity-50 backdrop-blur-md absolute top-20 right-0 mx-4 my-2 min-w-[200px] min-h-[100px] rounded-xl flex-col space-y-5 text-white font-bold`}>
-            <Link href="/" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-white hover:text-black text-center">Home</Link>
-            <Link href="/history" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-white hover:text-black text-center">History</Link>
-            <Link href="/about" className="bg-[#DA343E] px-[20px] py-[5px] rounded-lg hover:bg-white hover:text-black text-center">About</Link>
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-[#000000] bg-opacity-50 backdrop-blur-md absolute top-20 right-0 mx-4 my-2 min-w-[200px] min-h-[100px] rounded-xl flex-col space-y-5 text-white font-bold popup`}>
+            <Link href="/" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-[#6a70c8]  text-center">Home</Link>
+            <Link href="/history" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-[#6a70c8]  text-center">History</Link>
+            <Link href="/about" className="bg-[#DA343E] px-[20px] py-[5px] rounded-lg hover:bg-[#6a70c8]  text-center">About</Link>
           </div>
       </div>
     </nav>
