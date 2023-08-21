ccc2a0d61fde847cd8d7f841db9a1c96d6da9220
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-08-21 02:39:40 +0100

    working on mobile home page and why height doest not apply with tailwind and it does work with css custom

--- srcs/front-end/src/app/page.tsx
+++ srcs/front-end/src/app/page.tsx
@@ -1,19 +1,46 @@
 import HomePageNav from "../components/HomePage/HomePageNav"
+import { Press_Start_2P } from "next/font/google";
+import { Kanit } from "next/font/google";
+
+const pixelfont = Press_Start_2P({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
+
+const quotefont = Kanit({
+  subsets: ['latin'],
+  weight: ['400'],
+});
 
 export default function Home() {
   return (
-    <div className="bg-black overflow-hidden w-full h-full">
+    <div className="bg-[#0D0149] overflow-hidden w-full h-full">
       <HomePageNav />
-      <main className="w-full overflow-auto">
-        <h2 className="text-white text-center mt-[50px] font-bold">Retro</h2>
-        <h2 className="text-white text-center font-bold">Ping Pong</h2>
+      <main className="w-full h-75 custom">
+
+
+
+        {/* this is mobile */}
+        <div className="sm:hidden mt-10 flex items-center justify-between flex-col w-full h-full">
+            <div className="bg-[#DA343E] p-[15px] -rotate-12 rounded-md card-shadow w-fit h-fit">
+              <h2 className={`text-white text-center font-bold text-[50px]  ${pixelfont.className}`}>Retro</h2>
+              <h2 className={`text-white text-center font-bold text-[20px] ${pixelfont.className}`}>Ping Pong</h2>
+            </div>
+          <div className=" justify-self-end p-[15px] w-80 text-center mx-[20px]  rounded-md text-[#0D0149] bg-white font-bold ">Login with intra 42</div>
+
+
+        </div>
+
+
+
+
+
       </main>
     </div>
   );
 }
 
-// bg-[#0D0149]
 
-
-// TODO: search how to import fonts from google fonts using react13 tailwindcss
 // build home pages responsive for mobile
+// TODO: working on mobile home page and why height doest not apply with tailwind and it does work with css custom
--- srcs/front-end/src/components/HomePage/HomePageNav.tsx
+++ srcs/front-end/src/components/HomePage/HomePageNav.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { text } from "stream/consumers";
 
 export default function HomePageNav() {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -12,20 +11,18 @@ export default function HomePageNav() {
 
 
   return (
-    <nav className="bg-[#0D0149]  w-full h-[100px] flex justify-between items-center">
+    <nav className="bg-[#0D0149]  w-full h-25 flex justify-between items-center">
       <Image src="/myWhiteLogo.png" width={129.5} height={105.7} alt="Logo" priority={true} className="px-[15px] lg:[50px]"/>
       <div className="lg:flex hidden items-center space-x-20 text-white ml-[50px] mr-[20px] font-bold">
         <Link href="/" className="bg-[#333989] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#6a70c8] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#333989]">Home</Link>
         <Link href="/history" className="bg-[#333989] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#6a70c8] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#333989]">History</Link>
         <Link href="/about" className="bg-[#DA343E] px-[60px] py-[10px] rounded-lg duration-[0.4s] ease-in hover:bg-[#e36870] hover:scale-110 hover:shadow-[2px_1px_15px_1px_#DA343E]">About</Link>
       </div>
-      <div className="lg:hidden flex flex-1 justify-end items-center">
-        {/* TODO: I will update this button to make me log in with intra */}
-          <button className="text-white font-extrabold text-lg hover:text-[#cccccc]">Let's Go!</button>
+      <div className="lg:hidden flex flex-1 justify-end items-center ">
           <div className="mr-[15px] ml-[50px] w-[50px]">
             <Image src={toggle ? close : open} width={toggle ? 32 : 45} height={toggle ? 32 : 45} alt="Menu" className="object-contain" onClick={() => setToggle((prev) => !prev)} />
           </div>
-          <div className={`${toggle ? 'flex  popup' : ' flex hide-popup'} p-6 bg-[#000000] bg-opacity-50 backdrop-blur-md absolute top-20 right-0 mx-4 my-2 min-w-[200px] min-h-[100px] rounded-xl flex-col space-y-5 text-white font-bold`}>
+          <div className={`${toggle ? 'flex  popup' : 'hidden'} p-6 bg-[#000000] bg-opacity-50 backdrop-blur-md absolute top-20 right-0 mx-4 my-2 min-w-[200px] min-h-[100px] rounded-xl flex-col space-y-5 text-white font-bold z-20`}>
             <Link href="/" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-[#6a70c8]  text-center">Home</Link>
             <Link href="/history" className="bg-[#333989] px-[20px] py-[5px] rounded-lg hover:bg-[#6a70c8]  text-center">History</Link>
             <Link href="/about" className="bg-[#DA343E] px-[20px] py-[5px] rounded-lg hover:bg-[#e36870]  text-center">About</Link>
