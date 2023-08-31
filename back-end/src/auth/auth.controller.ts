551343aeac7131ed57732e52fa352b3192dec8b9
Abdellah BELLAKRIM <bellakrim2032@gmail.com>
2023-08-31 19:00:45 +0100

    database is well tested

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,31 +1,39 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(private readonly service: PrismaService){}
 
     // get /auth/login
     // this is the route users will use to authenticate
     @Get('login')
-    login(){
-        return 'login page';
-    };
+    async login(){
+        const newUser = await this.service.user.create(
+         {
+               data:{
+                email: "hello@example.com", username: 'example'
+            }
+        });
+        return newUser;
+        }
+};
 
-    //get /auth/redirect
-    // this is the redirect url the Oauth2 provider will call
-    @Get('redirect')
-    redirect(){
-        return 'change data page';
-    };
+    // //get /auth/redirect
+    // // this is the redirect url the Oauth2 provider will call
+    // @Get('redirect')
+    // redirect(){
+    //     return 'change data page';
+    // };
 
 
-    // get /auth/status
-    // retrive the auth status
-    @Get('status')
-    status(){};
+    // // get /auth/status
+    // // retrive the auth status
+    // @Get('status')
+    // status(){};
 
-    // get /auth/logout
-    // logging the user out
-    @Get('logout')
-    logout(){};
-}
+    // // get /auth/logout
+    // // logging the user out
+    // @Get('logout')
+    // logout(){};
