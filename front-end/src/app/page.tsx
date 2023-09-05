5936b317e94554204779b35d8d80872a5bd863b6
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-09-05 22:51:45 +0100

    final update from laptop

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -3,7 +3,7 @@ import HomePageNav from "../components/HomePage/HomePageNav";
 
 export default function Home() {
   return (
-    <div className="w-full h-full overflow-y-auto">
+    <div className="bg-[#0D0149] w-full h-full overflow-y-auto">
       <HomePageNav />
       <main className="w-full z-0">
         <HomePage />
