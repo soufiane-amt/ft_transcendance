1a4cb3990eba31b1bc9629ce496bd7e541e15c6f
Soufiane Amajat <samajat@e3r11p6.1337.ma>
2023-09-05 19:01:23 +0100

    some problems with image dimentions

--- components/shared/Message/Message.tsx
+++ components/shared/Message/Message.tsx
@@ -1,20 +1,39 @@
 import style from "./Message.module.css";
+import Image from 'next/image'
 
-function MessageBubble() {
+// const messageContent = "Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!"
+const messageContent = "Hello world!"
+const avatar = "/images/avatar.png"
+const isUserOnline = true;
+
+function MessageBubble({messageContent :string}) {
   return (
-    <div className={`${style.left} ${style.message_bubble} ${style.message_bubble___sender_theme}`}>
-    {/* <div className={`${style.right} ${style.message_bubble} ${style.message_bubble___receiver_theme}`}> */}
-      Hello world!
-      {/* Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world! */}
-      {/* Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world! */}
+    <div className={`${style.left} ${style.message_bubble} ${style.message_bubble___sent_style}`}>
+      {/* <div className={`${style.right} ${style.message_bubble} ${style.message_bubble___received_style}`}> */}
+      {messageContent}
     </div>
   );
+}
+
+
+function    AvatarUser ({messageOwnerAvatar, userVisibiliy})
+{
+    return (
+        <div >
+            <Image src={messageOwnerAvatar} alt="user avatar" width={50} height={50}/>
+            <Image src={userVisibiliy} alt="visibility icon"/>
+        </div>
+    )
 }
 
 export default function Message() {
   return (
     <div className={style.middlePosition}>
-      <MessageBubble />
+        <AvatarUser messageOwnerAvatar={avatar} userVisibiliy={isUserOnline} />
+        <div>
+            <MessageBubble messageContent={messageContent}/>
+            <div>16:35</div>
+        </div>
     </div>
   );
 }
