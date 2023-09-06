40f5b6cd4bff4e3ee134a0baa01e7d9ba09bf77f
Soufiane Amajat <samajat@e3r11p7.1337.ma>
2023-09-06 10:19:02 +0100

    Add visibility icon

--- components/shared/Message/Message.tsx
+++ components/shared/Message/Message.tsx
@@ -8,8 +8,8 @@ const isUserOnline = true;
 
 function MessageBubble({messageContent :string}) {
   return (
-    <div className={`${style.left} ${style.message_bubble} ${style.message_bubble___sent_style}`}>
-      {/* <div className={`${style.right} ${style.message_bubble} ${style.message_bubble___received_style}`}> */}
+    // <div className={`${style.left} ${style.message_bubble} ${style.message_bubble___sent_style}`}>
+      <div className={`${style.right} ${style.message_bubble} ${style.message_bubble___received_style}`}>
       {messageContent}
     </div>
   );
@@ -19,9 +19,9 @@ function MessageBubble({messageContent :string}) {
 function    AvatarUser ({messageOwnerAvatar, userVisibiliy})
 {
     return (
-        <div >
-            <Image src={messageOwnerAvatar} alt="user avatar" width={50} height={50}/>
-            <Image src={userVisibiliy} alt="visibility icon"/>
+        <div className={style.message_header}>
+            <Image className={`${style.message_header__avatar} ${style.image__full_circle}`} src={messageOwnerAvatar} alt="user avatar" width={100} height={1000}/>
+            <div className={`${style.message_header__visibility_icon} ${style.image__full_circle}`}/>
         </div>
     )
 }
@@ -30,10 +30,10 @@ export default function Message() {
   return (
     <div className={style.middlePosition}>
         <AvatarUser messageOwnerAvatar={avatar} userVisibiliy={isUserOnline} />
-        <div>
+        {/* <div>
             <MessageBubble messageContent={messageContent}/>
             <div>16:35</div>
-        </div>
+        </div> */}
     </div>
   );
 }
