aa1e46e5f7ed04304d6478d3bd89a6398cbf30fe
Soufiane Amajat <samajat@e3r11p8.1337.ma>
2023-09-10 17:37:33 +0100

    fetching data for bar is working properly

--- components/direct-messaging/DirectMsgMain.tsx
+++ components/direct-messaging/DirectMsgMain.tsx
@@ -1,18 +1,35 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatTextBox from "../shared/ChatTextbox/ChatTextbox";
 import DiscussionPanel from "../shared/DiscussionPanel/DiscussionPanel";
 import Message from "../shared/Message/Message";
 import style from "./DirectMsgMain.module.css"
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { CONFIG_FILES } from "next/dist/shared/lib/constants";
+import dataFile from '../../data/dataBar.json'
 
 
+type Discussion = {
+  id : string
+  user1_id :string
+  user2_id :string
+  status : 'ALLOWED' | 'BANNED'
+  createdAt: string
+  updatedAt: string
+}
 
-function DiscussionsBar ()
+function DiscussionsBar ({discussionPanels })
 {
+
   return (
-    <div className={style.discussion_panel_bar}>
-      <DiscussionPanel/>
-    </div>
+    <ul className={style.discussion_panel_bar}>
+      {
+        discussionPanels.map ( (panelElement) => {
+          return <DiscussionPanel DiscussionPanel={panelElement} />
+      })
+      }
+      {/* <DiscussionPanel DiscussionPanel={discussionPanels[0]} /> */}
+    </ul>
   )
 }
 
@@ -20,7 +37,7 @@ function MessagesHistory ()
 {
   return (
     <div className={style.messages_history}>  
-      <Message sentMessage={true}/>
+      {/* <Message sentMessage={true}/> */}
     </div>
   )
 }
@@ -35,11 +52,30 @@ function ChattingField ()
 
   )
 }
+
+// const rooms_data = [{      
+//               id: "1",
+//               user1_id: "user1",
+//               user2_id: "user2",
+//               status: "SENT",
+//               createdAt: "2023-09-10T08:00:00Z",
+//               updatedAt: "2023-09-10T08:00:00Z"}]
+
 function DirectMesgMain()
 {
+    const [rooms_data, setRoom] = useState([])
+    useEffect(() => {
+      async function fetchDataAsync() {
+          const result = await fetch('data/dataBar.json');
+          const data = await result.json();
+          setRoom(data);
+      }
+        fetchDataAsync();
+      }, [])
+  
     return (
         <div className={style.direct_msg_main}>
-          <DiscussionsBar/>
+          <DiscussionsBar discussionPanels={rooms_data} />
           <ChattingField/>
         </div>
     )
