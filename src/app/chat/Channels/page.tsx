fbd40417ef4fce6011189b3bf1b4e6cc1c5be38a
Soufiane Amajat <samajat@e3r9p10.1337.ma>
2023-10-05 18:40:31 +0100

    Add moderation modal

--- src/app/chat/Channels/page.tsx
+++ src/app/chat/Channels/page.tsx
@@ -1,41 +1,78 @@
 'use client'
 
+import { Flow_Rounded } from "next/font/google"
 import ChannelsMain from "../../components/Channels/ChannelsMain"
 import { SessionUserProvider } from "../../context/SessionUserContext"
 
 
+import React, {  useState } from "react";
 
 
-
-
-export default function Channels() {
-  
-    
+const Hello = ()=>{
   return (
-        <SessionUserProvider>
-            <ChannelsMain/>
-        </SessionUserProvider>
-    )
+    <div style={{background:"red"}}>
+        Hello world
+    </div>
+  )
 }
 
-// import { useEffect } from "react"
-// import socket from "../../socket/socket"
-// import { SocketProvider } from "../../context/tmSocket"
+const Modal = ()=>{
+  return (
+    <div className="modal">
+      <Tabs>
+          <div data_label="Option1">salam</div>
+          <div data_label="Option2">
+            <Hello/>
+          </div>
+          <div data_label="Option3">hola</div>
+      </Tabs>
 
+    </div>
+  )
+}
 
-// export default function Lol() {
+const Tabs = ({children})=>{
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <div className="body">
+      <div className="tabs">
+      {children.map((child, index) => (
+          <Tab
+            key={index}
+            index={index}
+            activeTab= {activeTab}
+            setActiveTab={setActiveTab}
+            data_label={child.props.data_label}
+          />
+        ))}
+        <div>
+          {
+            children[activeTab]
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Tab = ({index, activeTab, setActiveTab,  data_label})=>{
+
+  console.log(index)
+  const handleClick = ()=>{
+    setActiveTab(index)
+  }
+  return (
+      <div  className={index === activeTab ? "tab tab_active" : "tab"} onClick={handleClick}>
+          {data_label}
+      </div>
+  )
+}
+export default function Channels() {
   
-//     useEffect( ()=>{
-        
-//         socket.on ("newMessage", ()=>{
-//             console.log ("slm")
-//             console.log ("--------Socket-------", socket.id)
-//         })
-//     },[])
+    // <SessionUserProvider>
+    //     <ChannelsMain/>
+    // </SessionUserProvider>
     
-//   return (
-//     <>
-//             hello world
-//     </>
-//     )
-// }
+  return (
+    )
+}
