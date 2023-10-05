238791533ad799a89e0ae347fde564dbf99dd283
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-10-05 14:08:33 +0100

    handle circles animation for the hole application

--- front-end/src/app/Structure.tsx
+++ front-end/src/app/Structure.tsx
@@ -3,14 +3,14 @@
 import withAuth from "@/components/GlobalComponents/HigherOrderComponent";
 import NavBar from "@/components/GlobalComponents/ProfileNavBar/NavBar";
 import React from "react";
-import '../styles/TailwindRef.css'
+// import '../styles/TailwindRef.css'
 
 
 
 const Structure = ({ children }: { children: React.ReactNode }) => {
 
   return (
-    <main className="w-[100vw] h-[100vh] overflow-auto">
+    <main>
       {/* <NavBar /> */}
       <div className="h-[91px] bg-white w-full">this is nav</div>
       {children}
--- front-end/src/components/HomePage/BackgroundCircle.tsx
+++ front-end/src/components/HomePage/BackgroundCircle.tsx
@@ -15,10 +15,17 @@ function BackgroundCircle() {
         duration: 5.5,
       }}
     >
-      <div className="absolute  opacity-25 bg-[#DA343E] rounded-full h-[800px] w-[800px] animate-ping"></div>
+      {/* <div className="absolute  opacity-25 bg-[#DA343E] rounded-full h-[800px] w-[800px] animate-ping"></div>
       <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[600px] w-[600px] animate-ping"></div>
       <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[400px] w-[400px] animate-ping"></div>
       <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[200px] w-[200px] animate-ping"></div>
+      <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[200px] w-[200px]"></div>
+      <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[300px] w-[300px]"></div> */}
+
+      <div className="absolute border-[1px] border-[#DA343E] border-solid  rounded-full h-[800px] w-[800px] animate-ping"></div>
+      <div className="absolute opacity-[60%]  border-[1px] border-white border-solid rounded-full  h-[600px] w-[600px] animate-ping"></div>
+      <div className="absolute opacity-[60%]  border-[1px] border-white border-solid  rounded-full h-[400px] w-[400px] animate-ping"></div>
+      <div className="absolute opacity-[60%]  border-[1px] border-white border-solid  rounded-full h-[200px] w-[200px] animate-ping"></div>
       <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[200px] w-[200px]"></div>
       <div className="absolute  opacity-20 bg-slate-500 rounded-full h-[300px] w-[300px]"></div>
     </motion.div>
