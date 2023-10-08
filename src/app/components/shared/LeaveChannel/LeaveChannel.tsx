204eed95e6f5bfa4e854e88570183bcfe38164ff
Soufiane Amajat <samajat@e2r4p1.1337.ma>
2023-10-08 18:51:30 +0100

    Addcustumizable leave channel section is in process

--- src/app/components/shared/LeaveChannel/LeaveChannel.tsx
+++ src/app/components/shared/LeaveChannel/LeaveChannel.tsx
@@ -1,12 +1,30 @@
 
+import { useState } from "react";
 import style from "./LeaveChannel.module.css";
 
 export function LeaveChannel ()
 {
+    const [showDropDownList, setShowDropDownList] = useState(false)
+
+    const handleClickDropDown = (()=>{
+        setShowDropDownList(!showDropDownList)
+    })
     return (
         <div className={style.channel_quiting_section}>
             <h3>This section provide a way to quit the channel.</h3>
-            <button>Leave Channel</button>
+            <button onClick={handleClickDropDown}>Dropdown</button>
+            
+            <div >
+             {showDropDownList && 
+                <li>
+                    <ul>user1</ul>
+                    <ul>user1</ul>
+                    <ul>user1</ul>
+                </li>}
+            </div>
+
+            <button>Leave Channel</button> 
+            {/* Add tranfer and leave button */}
         </div>
     )
 }
