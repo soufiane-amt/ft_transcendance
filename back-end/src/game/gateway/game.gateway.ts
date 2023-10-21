ae54175784f06be269338fd89e5b985b32001dea
Ziko909 <aabou.zakaria909@gmail.com>
2023-10-21 22:12:37 +0100

    feat : check if the user is IN_GAME or if it is already joined the queue and add leave MatchMaking listener

--- back-end/src/game/gateway/game.gateway.ts
+++ back-end/src/game/gateway/game.gateway.ts
@@ -6,6 +6,9 @@ import { GatewaysGuard } from '../guards/gateways.guard';
 import { GameService } from '../game.service';
 import ClientSocket from '../interfaces/clientSocket.interface';
 import MatchMakingDto from '../dto/MatchMaking.dto';
+import LeaveQueueDto from '../dto/LeaveQueue.dto';
+import { Status } from '@prisma/client';
+import { UserCrudService } from 'src/prisma/user-crud.service';
 
 @UseGuards(GatewaysGuard)
 @WebSocketGateway({
@@ -13,7 +16,7 @@ import MatchMakingDto from '../dto/MatchMaking.dto';
                     origin : process.env.FRONT_SERV
                   })
 export class GameGateway implements OnGatewayInit<Server>, OnGatewayConnection<ClientSocket> {
-  constructor(private readonly gameservice: GameService) {}
+  constructor(private readonly gameservice: GameService, private readonly userCrudService: UserCrudService) {}
 
   afterInit(server: Server) {
     server.use(socketIOMiddleware);
@@ -25,8 +28,23 @@ export class GameGateway implements OnGatewayInit<Server>, OnGatewayConnection<C
 
   @SubscribeMessage('matchMaking')
   async matchmakingListener(@MessageBody() payload: string,@ConnectedSocket() client: ClientSocket): Promise<string> {
+    console.log(client.player);
     const game_settings : MatchMakingDto = JSON.parse(payload);
+    const user_state : Status = await this.userCrudService.getUserStatus(client.userId);
+    if (user_state === Status.IN_GAME) {
+      return "can't join the queue";
+    }
+    if (this.gameservice.playerExist(client.player) == false) {
+      return "can't join the queue";
+    }
     const response = await this.gameservice.joinMatchMackingSystem(client.player, game_settings);
     return response;
+  }
+
+  @SubscribeMessage('leave MatchMackingSystem')
+  handleLeaveMatchMackingSystem(@MessageBody() payload: string, @ConnectedSocket() client: ClientSocket) {
+    const { role } : LeaveQueueDto = JSON.parse(payload);
+    this.gameservice.removePlayerFromTheQueue(role, client.player);
+    return 'the player was removed from the queue';
   }
 }
