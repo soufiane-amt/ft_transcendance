4f2a2794effb9866898c9d50f3affda8c389652c
Ziko909 <aabou.zakaria909@gmail.com>
2023-10-21 18:40:35 +0100

    feat : create the game model

---
+++ back-end/src/game/game.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GameGateway } from './gateway/game.gateway';
+import { GameService } from './game.service';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { GatewaysGuard } from './guards/gateways.guard';
+import { UserCrudService } from 'src/prisma/user-crud.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [GameGateway, GameService,
+    {
+      provide: APP_GUARD,
+      useClass: GatewaysGuard
+    },
+]
+})
+export class GameModule {}
