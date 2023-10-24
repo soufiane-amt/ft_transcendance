938397e1b2fc0287ec7cd7f137992daa3f75a590
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-10-24 15:20:14 +0100

    update some code for build

--- front-end/src/components/game/InfoModel.tsx
+++ front-end/src/components/game/InfoModel.tsx
@@ -36,14 +36,14 @@ function Info({ ...props }) {
           <div>
             <h3 className={` ${mono.className}`}>Info:</h3>
             <p className={` ${mono.className}`}>
-              Welcome to our thrilling ping pong game! Whether you're a casual
-              player or a seasoned pro, this is the perfect platform to showcase
-              your skills and have a blast. We offer three exciting game modes
-              to choose from: "Practice Mode" for solo practice against
-              responsive AI opponents, "Invite Friends Mode" to challenge your
-              friends, and "Matchmaking Mode" for engaging matches within our
-              vibrant community. Grab your paddle and get ready to experience
-              the excitement of virtual ping pong!
+              {`            Welcome to our thrilling ping pong game! Whether you're a casual
+            player or a seasoned pro, this is the perfect platform to showcase
+            your skills and have a blast. We offer three exciting game modes
+            to choose from: "Practice Mode" for solo practice against
+            responsive AI opponents, "Invite Friends Mode" to challenge your
+            friends, and "Matchmaking Mode" for engaging matches within our
+            vibrant community. Grab your paddle and get ready to experience
+            the excitement of virtual ping pong!`}
             </p>
           </div>
           <div>
@@ -57,24 +57,24 @@ function Info({ ...props }) {
             </p>
             <h3 className={`${mono.className}`}>Game Rules:</h3>
             <p className={` ${mono.className}`}>
-              The game is played on a rectangular court with two paddles and a
+              {`              The game is played on a rectangular court with two paddles and a
               ball. You control one paddle (either with arrow keys or the
               mouse), while the computer controls the other. The game starts
               with the ball in the center of the court. Your goal is to prevent
               the ball from getting past your paddle while trying to score by
               getting the ball past your opponent's paddle. When the ball passes
               your opponent's paddle and goes off the screen on their side, you
-              earn a point. The first player to reach 5 points wins the game.
+              earn a point. The first player to reach 5 points wins the game.`}
             </p>
             <h3 className={`${mono.className}`}>Tips:</h3>
             <p className={` ${mono.className}`}>
-              Try to predict the ball's trajectory and position your paddle
-              accordingly to hit it. Don't let the ball get past your paddle, as
-              that will result in your opponent scoring a point. Focus on both
-              defense and offense, as you need to both prevent your opponent
-              from scoring and try to score points yourself. Pong can be a
-              fast-paced game, so be quick with your reactions. Enjoy playing
-              Pong, and may the best player win!
+              {`            Try to predict the ball's trajectory and position your paddle
+            accordingly to hit it. Don't let the ball get past your paddle, as
+            that will result in your opponent scoring a point. Focus on both
+            defense and offense, as you need to both prevent your opponent
+            from scoring and try to score points yourself. Pong can be a
+            fast-paced game, so be quick with your reactions. Enjoy playing
+            Pong, and may the best player win!`}
             </p>
           </div>
         </div>
