423e7465f88642c2911d7aabf8aada67c3e502fc
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-10-26 21:46:06 +0100

    setup globale component for game request with  timer

--- back-end/src/game/game.module.ts
+++ back-end/src/game/game.module.ts
@@ -5,15 +5,18 @@ import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { GatewaysGuard } from './guards/gateways.guard';
 import { UserCrudService } from 'src/prisma/user-crud.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { GameController } from './game.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   providers: [GameGateway, GameService,
     {
       provide: APP_GUARD,
       useClass: GatewaysGuard
     },
 ],
-exports: [GameService]
+exports: [GameService],
+controllers: [GameController]
 })
 export class GameModule {}
