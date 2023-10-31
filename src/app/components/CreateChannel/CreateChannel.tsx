5ed4265d89eaf44bc42fcdbdfbf967ceeaf40fe7
soufiane-amt <soufianeamt11@gmail.com>
2023-10-31 14:05:21 +0100

    Add characters limitations in inputs

--- src/app/components/CreateChannel/CreateChannel.tsx
+++ src/app/components/CreateChannel/CreateChannel.tsx
@@ -3,19 +3,44 @@ import { useRef, useState } from 'react';
 import style from './CreateChannel.module.css';
 import UploadChannelIcon from '../../../../public/images/icons/CreateChannel/UploadChannelIcon.jpg'
 import { ChannelInvitor } from './ChannelInvitor/ChannelInvitor';
+import { stringifyCookie } from 'next/dist/compiled/@edge-runtime/cookies';
+
+
+const MaxChannelNameLength = 15;
+const MinChannelNameLength = 3;
+const MaxPasswordLength = 50;
+const MinPasswordLength = 8;
 
 export function CreateChannel() {
 
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const [channelName, setChannelName] = useState<string | undefined>(undefined);
+    const [password, setPassword] = useState<string | undefined>('');
     const [selectedOption, setSelectedOption] = useState("PUBLIC");
-    const [newPassword, setNewPassword] = useState('');
     const [displayChannelInvitor, setDisplayChannelInvitor] = useState <boolean> (false)
+
+    const handleChannelNameChange = (e :any) => {
+        const inputText = e.target.value;
+        if ( inputText.length <= MaxChannelNameLength &&
+            inputText[inputText.length - 1] !== ' ') {
+            setChannelName(inputText);
+        }
+      };
+    
+      const handlePasswordChange = (e :any) => {
+        const inputText = e.target.value;
+        if ( inputText.length <= MaxPasswordLength &&
+            inputText[inputText.length - 1] !== ' ') {
+            setPassword(inputText);
+        }
+      };
+    
     const handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedOption(event.target.value);
     };
     
     const handlePasswordTyping = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewPassword(event.target.value);
+        setPassword(event.target.value);
     };
 
     const handleClickUpload  = () => {
@@ -35,7 +60,14 @@ export function CreateChannel() {
             </div>
             <div className={style.create_channel__name}>
                 <label>Channel name :</label>
-                <input type="text"  placeholder='Type in the channel name ...' />
+                <input value={channelName} type="text" onChange={handleChannelNameChange} placeholder='Type in the channel name ...' maxLength={MaxChannelNameLength}/>
+                {
+                    channelName && channelName.length < MinChannelNameLength &&
+                    <p className={style.create_channel_warning_message}>
+                        Your channel name must be at least {MinChannelNameLength} characters long!
+                    </p>
+
+                }
             </div>
             <div className={style.create_channel__select}>
                 <label>Choose the type of the channel :</label>
@@ -52,7 +84,16 @@ export function CreateChannel() {
                 <>
                     <label>Channel password :</label>
                     <div className={style.create_channel__password}>
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" 
+                            value={password}
+                            onChange={handlePasswordChange}/>
+                        {
+                         password && password.length < MinPasswordLength &&
+                        <p className={style.create_channel_warning_message}>
+                            Your password must be at least {MinPasswordLength} characters long!
+                        </p>
+
+                        }
                     </div>
                 </>
                 )
