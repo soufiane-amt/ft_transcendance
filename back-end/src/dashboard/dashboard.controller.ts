6c3baf69afeb2f9930ae09228863f5868e175e7a
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-11-05 16:16:22 +0100

    handle dashboard errors history and statistics

--- back-end/src/dashboard/dashboard.controller.ts
+++ back-end/src/dashboard/dashboard.controller.ts
@@ -1,48 +1,68 @@
-import { Body, Controller, Get, Patch, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { GameCrudService } from 'src/prisma/game-crud.service';
 import { UserCrudService } from 'src/prisma/user-crud.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-aut.guard';
 import { AuthService } from 'src/auth/auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-
 @Controller('api')
 export class DashboardController {
-    constructor(private readonly user : UserCrudService, private readonly resultgame : GameCrudService, private readonly authservice: AuthService, private readonly service: PrismaService) {}
-    @Get('Dashboard')
-    @UseGuards(JwtAuthGuard)
-    async HandleProfilepic(@Req() request, @Res() response: any) {
-      const authorizationHeader = request.headers.authorization;
-      if (!authorizationHeader) {
-        return response.status(401).send({ error: 'Authorization header is missing' });
-      }
-      const tokenParts = authorizationHeader.split(' ');
-      if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-        return response.status(401).send({ error: 'Invalid authorization header format' });
-      }
-
-      const JwtToken: string = tokenParts[1];
-    
-      const payload: any = this.authservice.extractPayload(JwtToken);
-      const user = await this.service.prismaClient.user.findUnique({
-        where: {
-          id: payload.userId,
-        },
-      });
-      return response.status(200).send(user);
-    }
-
-  @Get('Dashboard/allUsers')
+  constructor(
+    private readonly user: UserCrudService,
+    private readonly resultgame: GameCrudService,
+    private readonly authservice: AuthService,
+    private readonly service: PrismaService,
+  ) {}
+  @Get('Dashboard')
   @UseGuards(JwtAuthGuard)
-  async sendAllfriend(@Req() request, @Res() response: any)
-  {
+  async HandleProfilepic(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
+    }
+
+    const JwtToken: string = tokenParts[1];
+
+    const payload: any = this.authservice.extractPayload(JwtToken);
+    const user = await this.service.prismaClient.user.findUnique({
+      where: {
+        id: payload.userId,
+      },
+    });
+    return response.status(200).send(user);
+  }
+
+  @Get('Dashboard/allUsers')
+  @UseGuards(JwtAuthGuard)
+  async sendAllfriend(@Req() request, @Res() response: any) {
+    const authorizationHeader = request.headers.authorization;
+    if (!authorizationHeader) {
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
+    }
+    const tokenParts = authorizationHeader.split(' ');
+    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
@@ -52,11 +72,14 @@ export class DashboardController {
       const users: any[] = await this.user.findNonFriendsUsers(payload.userId);
       const newusers = await Promise.all(
         users.map(async (user) => {
-          const isFriendRequestSent = await this.user.FriendShipRequestAlreadySent(payload.userId, user.id);
-          if (isFriendRequestSent)
-            user.pending = true;
+          const isFriendRequestSent =
+            await this.user.FriendShipRequestAlreadySent(
+              payload.userId,
+              user.id,
+            );
+          if (isFriendRequestSent) user.pending = true;
           return user;
-        })
+        }),
       );
       return response.status(200).send(newusers);
     } catch (error) {
@@ -65,18 +88,21 @@ export class DashboardController {
       return response.status(500).send({ error: 'Internal Server Error' });
     }
   }
-  
+
   @Get('Dashboard/friends')
   @UseGuards(JwtAuthGuard)
-  async sendUser(@Req() request, @Res() response: any)
-  {
+  async sendUser(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
@@ -86,13 +112,12 @@ export class DashboardController {
       const usersId: any[] = await this.user.findFriendsList(payload.userId);
       // console.log("users ID : ", usersId);
       const users: any[] = [];
-    
+
       await Promise.all(
         usersId.map(async (user) => {
-            const userData = await this.user.findUserByID(user);
-            users.push(userData);
-          
-        })
+          const userData = await this.user.findUserByID(user);
+          users.push(userData);
+        }),
       );
       return response.status(200).send(users);
     } catch (error) {
@@ -104,22 +129,27 @@ export class DashboardController {
 
   @Get('Dashboard/friends/result')
   @UseGuards(JwtAuthGuard)
-  async result(@Req() request, @Res() response: any)
-  {
+  async result(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
 
     try {
       const payload: any = this.authservice.extractPayload(JwtToken);
-      const users: any[] = await this.user.userMatchsRecord(payload.userId);
+      const users: any[] = await this.resultgame.retreiveGamesScoreForStatistic(
+        payload.userId,
+      );
       return response.status(200).send(users);
     } catch (error) {
       // Handle any errors that occur during the process
@@ -130,15 +160,18 @@ export class DashboardController {
 
   @Get('Dashboard/game')
   @UseGuards(JwtAuthGuard)
-  async game(@Req() request, @Res() response: any)
-  {
+  async game(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
@@ -149,22 +182,26 @@ export class DashboardController {
 
   @Get('Dashboard/statistic')
   @UseGuards(JwtAuthGuard)
-  async sendStatistic(@Req() request, @Res() response: any)
-  {
+  async sendStatistic(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
 
     try {
       const payload: any = this.authservice.extractPayload(JwtToken);
-      const statistic: any[] = await this.resultgame.retreiveGamesScoreForStatistic(payload.userId);
+      const statistic: any[] =
+        await this.resultgame.retreiveGamesScoreForStatistic(payload.userId);
       return response.status(200).send(statistic);
     } catch (error) {
       // Handle any errors that occur during the process
@@ -175,44 +212,50 @@ export class DashboardController {
 
   @Post('Dashboard/logout')
   @UseGuards(JwtAuthGuard)
-  async Section(@Req() request, @Res() response: any)
-  {
-    if (request)
-    {
+  async Section(@Req() request, @Res() response: any) {
+    if (request) {
       const authorizationHeader = request.headers.authorization;
       if (!authorizationHeader) {
-        return response.status(401).send({ error: 'Authorization header is missing' });
+        return response
+          .status(401)
+          .send({ error: 'Authorization header is missing' });
       }
       const tokenParts = authorizationHeader.split(' ');
       if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-        return response.status(401).send({ error: 'Invalid authorization header format' });
+        return response
+          .status(401)
+          .send({ error: 'Invalid authorization header format' });
       }
-    // 
+      //
       const JwtToken: string = tokenParts[1];
-    // 
+      //
       const payload: any = this.authservice.extractPayload(JwtToken);
 
-      await this.user.changeVisibily(payload.userId, "OFFLINE");
+      await this.user.changeVisibily(payload.userId, 'OFFLINE');
     }
   }
   @Get('Dashboard/notification')
   @UseGuards(JwtAuthGuard)
-  async sendnotification(@Req() request, @Res() response: any)
-  {
+  async sendnotification(@Req() request, @Res() response: any) {
     const authorizationHeader = request.headers.authorization;
     if (!authorizationHeader) {
-      return response.status(401).send({ error: 'Authorization header is missing' });
+      return response
+        .status(401)
+        .send({ error: 'Authorization header is missing' });
     }
     const tokenParts = authorizationHeader.split(' ');
     if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-      return response.status(401).send({ error: 'Invalid authorization header format' });
+      return response
+        .status(401)
+        .send({ error: 'Invalid authorization header format' });
     }
 
     const JwtToken: string = tokenParts[1];
 
     try {
       const payload: any = this.authservice.extractPayload(JwtToken);
-      const notificationtable = await this.user.getUserNotificationsWithUser2Data(payload.userId);
+      const notificationtable =
+        await this.user.getUserNotificationsWithUser2Data(payload.userId);
       return response.status(200).send(notificationtable);
     } catch (error) {
       // Handle any errors that occur during the process
--- front-end/src/components/Dashboard/History.tsx
+++ front-end/src/components/Dashboard/History.tsx
@@ -1,62 +1,81 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
-function History()
-{
-    const [FriendHistory, setFriendHistory] = useState<{player_1_id: string; player_2_id: string; result: string}[]>([]);
-    const JwtToken = Cookies.get("access_token");
+function History() {
+  const [FriendHistory, setFriendHistory] = useState<
+    { player_1_id: string; player_2_id: string; result: string }[]
+  >([]);
+  const JwtToken = Cookies.get("access_token");
 
-    useEffect(() => {
-        fetch('http://localhost:3001/api/Dashboard/friends/result', {
-            method: 'GET',
-            headers: {
-              'Authorization': `Bearer ${JwtToken}`,
-              'Content-Type': 'application/json',
-            },
-          })
-            .then((response) => {
-                if (!response.ok)
-                    throw new Error('Network response was not ok');
-                return response.json();
-            })
-            .then((data) => setFriendHistory(data))
-            .catch((error) => {
-                console.error('Error:', error);
-              });
-    }, [JwtToken]);
-    return (
-        <>
-             <div className="informationaboutuser" >
-                {FriendHistory.map(history => {
-                    const [score_1, score_2] = history.result.split('-');
-                    const compositeKey = `${history.player_1_id}-${history.player_2_id}-${score_1}-${score_2}`;
-                    return (
-                    <div className="container-user friend" key={compositeKey}>
-                    <div className="information-user">
-                        <div>
-                        <img src="user.jpg" alt="photo"></img>
-                            <p>{history.player_1_id}</p>
-                        </div>
-                        {score_1 > score_2 && <img src="high-score.png" id="crown-score" width={40} height={40} alt="photo"></img>}
-                    </div>
-                    <div className="score-friend">
-                        <p>Score</p>
-                        <p><span>{score_1}</span>VS<span>{score_2}</span></p>
-                    </div>
-                    <div className="information-friend">
-                    {score_1 < score_2 && <img src="high-score.png"id="crown-score-friend" width={40} height={40} alt="photo"></img>}
-                        <div>
-                            <img src="images.png" alt="photo"></img>
-                            <p>{history.player_2_id}</p>
-                        </div>
-                    </div>
+  useEffect(() => {
+    fetch("http://localhost:3001/api/Dashboard/friends/result", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${JwtToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        return response.json();
+      })
+      .then((data) => {
+        setFriendHistory(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [JwtToken]);
+  return (
+    <>
+      <div className="informationaboutuser">
+        {FriendHistory.map((history) => {
+          const [score_1, score_2] = history.result.split("-");
+          const compositeKey = `${history.player_1_id}-${history.player_2_id}-${score_1}-${score_2}`;
+          return (
+            <div className="container-user friend" key={compositeKey}>
+              <div className="information-user">
+                <div>
+                  <img src="user.jpg" alt="photo"></img>
+                  <p>{history.player_1_id}</p>
                 </div>
-                )
-                })}
+                {score_1 > score_2 && (
+                  <img
+                    src="high-score.png"
+                    id="crown-score"
+                    width={40}
+                    height={40}
+                    alt="photo"
+                  ></img>
+                )}
+              </div>
+              <div className="score-friend">
+                <p>Score</p>
+                <p>
+                  <span>{score_1}</span>VS<span>{score_2}</span>
+                </p>
+              </div>
+              <div className="information-friend">
+                {score_1 < score_2 && (
+                  <img
+                    src="high-score.png"
+                    id="crown-score-friend"
+                    width={40}
+                    height={40}
+                    alt="photo"
+                  ></img>
+                )}
+                <div>
+                  <img src="images.png" alt="photo"></img>
+                  <p>{history.player_2_id}</p>
+                </div>
+              </div>
             </div>
-            
-        </>
-    );
-};
+          );
+        })}
+      </div>
+    </>
+  );
+}
 
 export default History;
