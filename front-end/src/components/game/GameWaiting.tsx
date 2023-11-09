e8c1168cf674248c6adbe5878a91d0102b5246cb
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-11-09 11:34:40 +0100

    update loading use same colors

--- front-end/src/components/game/GameWaiting.tsx
+++ front-end/src/components/game/GameWaiting.tsx
@@ -6,7 +6,7 @@ import LoadingAnimation from "@/../public/LoadingAnimation.json";
 function GameWaiting() {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-[40]">
-      <div className="h-full w-full bg-indigo-100 absolute flex items-center justify-center flex-col flex-wrap z-40 top-0 overflow-hidden">
+      <div className="h-full w-full bg-[#ccc4f7] absolute flex items-center justify-center flex-col flex-wrap z-40 top-0 overflow-hidden">
         <Lottie
           className="h-3/5"
           animationData={LoadingAnimation}
