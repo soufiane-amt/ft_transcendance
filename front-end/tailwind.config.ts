918def480c4c37306b40a3cf85b8b489912ac751
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-11-18 03:14:39 +0100

    update our tailwind config file

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./src/app/page.tsx",
+    "./src/app/updatecredentials/page.tsx",
+    "./src/app/2fa/page.tsx",
     "./src/components/HomePage/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/2fa/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/updatecredentials/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +15,8 @@ const config: Config = {
     "./src/app/game/page.tsx",
     "./src/components/game/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/GlobalComponents/GameInvitation.tsx",
+    "./src/components/GlobalComponents/GameChatSettings.tsx",
+    "./src/components/GlobalComponents/Loading.tsx",
   ],
   theme: {
     extend: {
