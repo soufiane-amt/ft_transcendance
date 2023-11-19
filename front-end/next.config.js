682193ed8452e33a64bf45fc8a9aa3c967e662e3
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-11-19 17:05:45 +0100

    fix depricated packages

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -2,28 +2,27 @@
 
 // next.config.js
 module.exports = {
-    // Other Next.js configuration options...
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
-    },
-  
-    // Enable TypeScript checking
-    typescript: {
-      // Optionally specify the type-checker version
-      ignoreBuildErrors: false, // Set to true to disable type-checking during production build
-    },
+  // Other Next.js configuration options...
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-    images: {
-      domains: ['localhost', 'cdn.intra.42.fr'], // Add any other domains as needed
-    },
-  
-  };
-  
+    return config;
+  },
+
+  // Enable TypeScript checking
+  typescript: {
+    // Optionally specify the type-checker version
+    ignoreBuildErrors: true, // Set to true to disable type-checking during production build
+  },
+
+  images: {
+    domains: ["localhost", "cdn.intra.42.fr"], // Add any other domains as needed
+  },
+};
+
 const nextConfig = {
   images: {
     domains: [
