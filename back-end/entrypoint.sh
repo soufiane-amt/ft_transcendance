5e0e44c7ad04b478a54702d60b1bf7b59d0272cb
AbdellahBellakrim <bellakrim2032@gmail.com>
2023-11-20 01:31:54 +0100

    finish dockerizing the app

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Install necessary packages
+apt-get -y update
+apt-get install -y postgresql-client 
 
-npx prisma migrate deploy
+# Function to check if the PostgreSQL database is ready
+check_postgresql() {
+  until pg_isready -h db -p 5432 -q; do
+    echo "Waiting for PostgreSQL database to start..."
+    sleep 1
+  done
+  echo "PostgreSQL database is ready!"
+}
 
+# Wait for PostgreSQL to start
+check_postgresql
+
+
+# Run the commands once the database is ready
+npx prisma migrate dev --name init
 npm run build
-
-
+npm run start
