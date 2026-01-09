ec805ceffed46ed69ed5ddc16652c25f71da9874
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-09 18:48:07 +0100

    fix: allow cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,13 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: process.env.FRONT_SERV,
     credentials: true,
-    // origin: '*',
-  });  
+  });
 
-
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
-     
