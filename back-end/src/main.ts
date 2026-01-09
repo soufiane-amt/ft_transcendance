362ad98f8389545c001605bda464333e4645a44c
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-09 19:08:32 +0100

    fix : add allows

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONT_SERV,
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
   });
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
