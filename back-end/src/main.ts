5785532d8537e7ed50d22699702630fe31c8376c
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-10 11:14:39 +0100

    feat: update listenining

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
   });
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
