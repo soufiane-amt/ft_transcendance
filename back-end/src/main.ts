3050e8cd401af6e71d3f68118b923fa2fb9172b9
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-10 11:24:17 +0100

    feat: update cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: process.env.FRONT_SERV,
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
