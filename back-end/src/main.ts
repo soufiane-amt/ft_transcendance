213a084c1d8c255e49e8f9b06ea431971cdde3b9
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-24 14:34:10 +0100

    fix: update port value

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3001;
 
-  await app.listen(port, '0.0.0.0');
+  await app.listen(8000, '0.0.0.0');
 
   console.log(`NestJS is running on port ${port}`);
 }
