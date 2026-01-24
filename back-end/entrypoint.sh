eed08f4ecb3041912979404901373f4d5318b580
Soufiane Amajat <66280941+soufiane-amt@users.noreply.github.com>
2026-01-24 13:58:00 +0100

    fix: comment unecessary commands

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -2,19 +2,19 @@
 
 # Install necessary packages
 apt-get -y update
-apt-get install -y postgresql-client 
+# apt-get install -y postgresql-client 
 
-# Function to check if the PostgreSQL database is ready
-check_postgresql() {
-  until pg_isready -h db -p 5432 -q; do
-    echo "Waiting for PostgreSQL database to start..."
-    sleep 1
-  done
-  echo "PostgreSQL database is ready!"
-}
+# # Function to check if the PostgreSQL database is ready
+# check_postgresql() {
+#   until pg_isready -h db -p 5432 -q; do
+#     echo "Waiting for PostgreSQL database to start..."
+#     sleep 1
+#   done
+#   echo "PostgreSQL database is ready!"
+# }
 
-# Wait for PostgreSQL to start
-check_postgresql
+# # Wait for PostgreSQL to start
+# check_postgresql
 
 
 # Run the commands once the database is ready
