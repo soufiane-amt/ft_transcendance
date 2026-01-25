90015bb8c741734975d4ae2539d850bd6b9c274b
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-25 14:48:57 +0100

    fix: update config 4

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://ft-transcendance-three.vercel.app",
+    origin: 'https://ft-transcendance-three.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
 
