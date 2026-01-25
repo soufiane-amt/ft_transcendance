52b25a408fee82ce7c17ea39f1244db0a7380433
Soufiane Amajat <soufianeamt11@gmail.com>
2026-01-25 14:22:08 +0100

    fix: update configuration

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,14 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: process.env.FRONT_SERV,
     credentials: true,
-    // origin: '*',
   });
 
   const port = process.env.PORT || 3001;
 
-  await app.listen(8000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 
   console.log(`NestJS is running on port ${port}`);
 }
